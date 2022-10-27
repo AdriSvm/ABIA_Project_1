c7acc3e0b188046c9c5738b1ccefca081d2296e5
AdriSvm <adria.svm@gmail.com>
2022-10-27 09:38:00 +0200

    exps.py complete

--- exps.py
+++ exps.py
@@ -79,6 +79,31 @@ def exp2():
 
 #print(exp2())
 
+def exp3():
+    df = pd.DataFrame()
+    seeds = [22,1234,52,393,387,672,812,193,999]
+    gains_inits = []
+    gains_fins = []
+    timmings = []
+    for i in seeds:
+        seed = i
+        print(f"Doing exp1 with seed {seed}, number {seeds.index(seed)+1} of {len(seeds)}")
+        initial_state, n = experiment('SIMULATED ANNEALING', 'ONLY GRANTED', [5, 10, 25], 1000, [0.2, 0.3, 0.5], 0.75, seed)
+        timming, nothing = experiment('SIMULATED ANNEALING', 'ONLY GRANTED', [5, 10, 25], 1000, [0.2, 0.3, 0.5], 0.75, seed, True, 10)
+        gains_init = initial_state.heuristic()
+        gains_fin = n.heuristic()
+        timming /= 10
+        gains_inits.append(gains_init)
+        gains_fins.append(gains_fin)
+        timmings.append(timming)
+    df["Seed"] = seeds
+    df["initial_gains"] = gains_inits
+    df["final_gains"] = gains_fins
+    df["mean_time"] = timmings
+    return df
+
+#print(exp3())
+
 def exp4():
     df = pd.DataFrame()
     seeds = [134,34,556,9012,4321,12,346,762,987,222]
@@ -108,4 +133,54 @@ def exp4():
     df["mean_time"] = timmings
     return df
 
-d = exp4()
+#d = exp4()
+
+
+def exp5():
+    df = pd.DataFrame()
+    seeds = [274, 691, 356, 732, 285, 22, 467, 89, 123, 34]
+    gains_inits = []
+    gains_fins = []
+    diff_gains = []
+    timmings = []
+    seed2 = []
+    pena = []
+    Llega = []
+    no_elec = []
+
+    for i in seeds:
+        for x in range(0, 2000, 200):
+            llega = False
+            seed = i
+            # initial_state, n = experiment('HILL CLIMBING', 'PROBLEM 5', [5, 10, 25], 1000, [0.2, 0.3, 0.5], 0.75, seed, False,x=x)
+            # timming, nothing = experiment('HILL CLIMBING', 'PROBLEM 5', [5, 10, 25], 1000, [0.2, 0.3, 0.5], 0.75, seed, True,5, x=x)
+            initial_state, n = experiment('SIMULATED ANNEALING', 'PROBLEM 5', [5, 10, 25], 1000, [0.2, 0.3, 0.5], 0.75,
+                                          seed, False, x=x)
+            timming, nothing = experiment('SIMULATED ANNEALING', 'PROBLEM 5', [5, 10, 25], 1000, [0.2, 0.3, 0.5], 0.75,
+                                          seed, True, 1, x=x)
+            if not n.encontrado_no_elec and not n.encontrado:
+                llega = True
+
+            gains_init = initial_state.heuristic()
+            gains_fin = n.heuristic()
+            diff_gain = gains_fin - gains_init
+            gains_inits.append(gains_init)
+            gains_fins.append(gains_fin)
+            diff_gains.append(diff_gain)
+            Llega.append(llega)
+            seed2.append(i)
+            pena.append(x)
+            timmings.append(timming)
+            no_elec.append(len(n.left))
+
+    df["Seed"] = seed2
+    df["Penalització"] = pena
+    df["Vàlido"] = Llega
+    df["No elec"] = no_elec
+    df["initial_gains"] = gains_inits
+    df["final_gains"] = gains_fins
+    df["mean_time"] = timmings
+    df["diff_gains"] = diff_gains
+    return df
+
+#print(exp5())
